5e1ba843504ae919de0a04c64ba0b1f9e9d3fa63
julnpires <juliananp@al.insper.edu.br>
2025-10-24 15:32:56 -0300

    4.0

--- funcoes.py
+++ funcoes.py
@@ -22,3 +22,35 @@ def faz_jogada(tabuleiro, linha, coluna):
     else:
         tabuleiro[linha][coluna] = '-'
     return tabuleiro
+
+def posiciona_frota(frota):
+    tabuleiro = [[0 for i in range(10)] for j in range(10)]
+    for navio in frota:
+        for posicoes in frota[navio]:
+            for posicao in posicoes:
+                linha, coluna = posicao
+                tabuleiro[linha][coluna] = 1
+    return tabuleiro
+
+frota = {
+    "porta-aviões":[
+      [[1,5],[1,6],[1,7],[1,8]]
+    ],
+    "navio-tanque":[
+      [[6,1],[6,2],[6,3]],
+      [[4,7],[5,7],[6,7]]
+    ],
+    "contratorpedeiro":[
+      [[1,1],[2,1]],
+      [[2,3],[3,3]],
+      [[9,1],[9,2]]
+    ],
+    "submarino": [
+      [[0,3]],
+      [[4,5]],
+      [[8,9]],
+      [[8,4]]
+    ],
+}
+resultado = posiciona_frota(frota)
+print(resultado)
