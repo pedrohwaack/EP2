026cc88b282e48163f0cb07e97237a9f354177d8
pedrohwaack <pedrohw@al.insper.edu.br>
2025-10-24 15:38:11 -0300

    4.1

--- funcoes.py
+++ funcoes.py
@@ -31,26 +31,3 @@ def posiciona_frota(frota):
                 linha, coluna = posicao
                 tabuleiro[linha][coluna] = 1
     return tabuleiro
-
-frota = {
-    "porta-aviões":[
-      [[1,5],[1,6],[1,7],[1,8]]
-    ],
-    "navio-tanque":[
-      [[6,1],[6,2],[6,3]],
-      [[4,7],[5,7],[6,7]]
-    ],
-    "contratorpedeiro":[
-      [[1,1],[2,1]],
-      [[2,3],[3,3]],
-      [[9,1],[9,2]]
-    ],
-    "submarino": [
-      [[0,3]],
-      [[4,5]],
-      [[8,9]],
-      [[8,4]]
-    ],
-}
-resultado = posiciona_frota(frota)
-print(resultado)
